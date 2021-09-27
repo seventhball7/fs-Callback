700a1ce50d5fc891cda40ebebf38cc148d1fb877
Sameer Srivastava <sameer.srivastava.17@mountblue.tech>
2021-09-27 16:39:49 +0530

    deleted part left

--- test/testProblem2.js
+++ test/testProblem2.js
@@ -1,33 +1,31 @@
 const ires = require("../problem2");
 
-const cb = (err, upperDatafilePath) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("file converted to upper case in " + upperDatafilePath);
-    ires.toLowerData(upperDatafilePath, (err, lowerDataFilePath) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("file converted to lower case in " + lowerDataFilePath);
-        ires.sortedFile(lowerDataFilePath, (err, sortedFilePath) => {
-          if (err) {
-            console.error(err);
-          } else {
-            console.log("file sorted in " + sortedFilePath);
-            ires.deleteFiles((err,pathtoDelete)=>{
-              if(err){
-                console.error(err);
-              }
-              else{
-                console.log("path deleted successfully from "+ pathtoDelete)
-              }
-            });
-          }
-        });
-      }
-    });
-  }
-};
+ires
+  .fileRead()
+  .then((data) => {
+    console.log("file path of lipsum data " + data);
+    return ires.toUpperCase(data);
+})
+.then((convertedUpperDataPath)=>{
+  console.log("converted to upper case at " +convertedUpperDataPath)
+  return ires.toLowerCase(convertedUpperDataPath);
+})
+.then((convertedLowerDataPath)=>{
+  console.log("converted to lower case at "+ convertedLowerDataPath);
+  return ires.sortTheFile(convertedLowerDataPath)
+})
+.then((sortedFilePath)=>{
+  console.log("sorted the files at "+ sortedFilePath)
+})
 
-ires.fileRead(cb);
+.catch((errors)=>{
+  console.log(errors)
+})
+
+  // ires.deleteAllFiles()
+  // .then((data)=>{
+  //  console.log("files deleted from "+ data);
+  // })
+  
+
+
