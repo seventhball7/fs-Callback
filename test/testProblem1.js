879559622f738c2a10ae16d198ec463dd1ea0d8b
Sameer Srivastava <sameer.srivastava.17@mountblue.tech>
2021-09-28 10:56:03 +0530

    problem1 solved

--- test/testProblem1.js
+++ test/testProblem1.js
@@ -1,15 +1,3 @@
 const ires = require("../problem1");
 
-ires
-  .createJSON()
-  .then((data) => {
-    console.log("files created are  " + data);
-    return ires.deleteJSON(data);
-  })
-  .then((result) => {
-    console.log("\n")
-    console.log("files deleted at  " + result);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+ires.createJSON();
